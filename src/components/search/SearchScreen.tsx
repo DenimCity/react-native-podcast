f19cd917dc2ee5454f1b1fde99af42ed64026d70
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-08 15:35:12 -0400

    added styling to  search input

--- src/components/search/SearchScreen.tsx
+++ src/components/search/SearchScreen.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import {Box, Text} from 'react-native-design-utility';
+import {Box} from 'react-native-design-utility';
+import {TextInput} from 'react-native-gesture-handler';
+import {StyleSheet} from 'react-native';
+import {theme} from '../../constants/theme';
 
 const SearchScreen = () => {
   return (
-    <Box f={1} center>
-      <Text>Search Screen</Text>
+    <Box f={1} bg="white">
+      <Box h={50} w="100%" px="sm" mt="sm">
+        <TextInput
+          style={s.input}
+          placeholder="Search Podcast"
+          selectionColor={theme.color.blueLight}
+        />
+      </Box>
     </Box>
   );
 };
 
+const s = StyleSheet.create({
+  input: {
+    height: 40,
+    flex: 1,
+    backgroundColor: theme.color.greyLightest,
+    borderRadius: 10,
+    paddingHorizontal: theme.space.sm,
+    fontSize: theme.text.size.md,
+  },
+});
 export default SearchScreen;
