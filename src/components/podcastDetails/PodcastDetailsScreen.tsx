d31f46f153613a502d025affbf21004a4340b9d6
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-11 10:38:37 -0400

    now pulling in feed url dynamically

--- src/components/podcastDetails/PodcastDetailsScreen.tsx
+++ src/components/podcastDetails/PodcastDetailsScreen.tsx
@@ -3,31 +3,46 @@ import {Box, Text} from 'react-native-design-utility';
 
 import {SearchStackRouteParamList} from '../../navigators/types';
 import {RouteProp, useRoute} from '@react-navigation/native';
-import {FlatList, Image, StyleSheet} from 'react-native';
+import {FlatList, Image, StyleSheet, ActivityIndicator} from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import {theme} from '../../constants/theme';
+import {useQuery} from '@apollo/client';
+import {FearchQuery, FearchQueryVariables} from '../../types/graphql';
+import feedQuery from '../../graphql/query/feedQuery';
 
 type NavigationParams = RouteProp<SearchStackRouteParamList, 'PodcastDetails'>;
 
 export default function PodcastDetailsScreen() {
-  const {data} = useRoute<NavigationParams>().params ?? {};
+  const {data: podcastData} = useRoute<NavigationParams>().params ?? {};
+  const {data, loading} = useQuery<FearchQuery, FearchQueryVariables>(
+    feedQuery,
+    {
+      variables: {
+        feedUrl: podcastData.feedUrl,
+      },
+    },
+  );
+
   return (
     <Box f={1} bg="white">
       <FlatList
         ListHeaderComponent={
           <>
             <Box dir="row" px="sm" mt="sm" mb="md">
-              {data.thumbnail && (
+              {podcastData.thumbnail && (
                 <Box mr={10}>
-                  <Image source={{uri: data.thumbnail}} style={s.thumbnail} />
+                  <Image
+                    source={{uri: podcastData.thumbnail}}
+                    style={s.thumbnail}
+                  />
                 </Box>
               )}
               <Box f={1}>
                 <Text size="lg" bold>
-                  {data.podcastName}
+                  {podcastData.podcastName}
                 </Text>
                 <Text size="xs" color="grey">
-                  {data.artist}
+                  {podcastData.artist}
                 </Text>
                 <Text color="blue" size="xs">
                   Subscribe
@@ -42,9 +57,9 @@ export default function PodcastDetailsScreen() {
                   color={theme.color.blueLight}
                 />
               </Box>
-              <Box>
+              <Box f={1}>
                 <Text bold>Play</Text>
-                <Text size="sm">#400 - The Last Episode</Text>
+                <Text size="sm">{data?.feed[0].title}</Text>
               </Box>
             </Box>
             <Box px="sm" mb="md">
@@ -52,30 +67,26 @@ export default function PodcastDetailsScreen() {
                 Episodes
               </Text>
             </Box>
+            {loading && (
+              <Box h={200} center>
+                <ActivityIndicator size="large" color={theme.color.blueLight} />
+              </Box>
+            )}
           </>
         }
-        data={[{id: '1'}, {id: '2'}]}
+        data={data?.feed}
         ItemSeparatorComponent={() => (
           <Box w="100%" px="sm" my="sm">
             <Box style={{height: StyleSheet.hairlineWidth}} bg="greyLighter" />
           </Box>
         )}
-        keyExtractor={(item) => item.id}
-        renderItem={() => (
+        keyExtractor={(item) => item.linkUrl}
+        renderItem={({item}) => (
           <Box px="sm">
             <Text size="small">Friday</Text>
-            <Text bold>#400 - The Title</Text>
+            <Text bold>{item.title}</Text>
             <Text size="sm" color="grey" numberOfLines={2}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+              {item.description}
             </Text>
             <Text size="sm" color="grey">
               3hrs. 13min
