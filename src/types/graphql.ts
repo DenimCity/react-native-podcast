3470a11c8a2cbc1e9ec6088aee80228928e0a9d2
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-11 10:38:04 -0400

    added feedQuerytypes

--- src/types/graphql.ts
+++ src/types/graphql.ts
@@ -4,10 +4,40 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
+// GraphQL query operation: FearchQuery
+// ====================================================
+
+export interface FearchQuery_feed {
+  __typename: "FeedItem";
+  pubDate: string;
+  text: string;
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string | null;
+  summary: string;
+  linkUrl: string;
+  duration: string;
+}
+
+export interface FearchQuery {
+  feed: FearchQuery_feed[];
+}
+
+export interface FearchQueryVariables {
+  feedUrl: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
 // GraphQL query operation: searchQuery
 // ====================================================
 
-export interface SearchQuery_search {
+export interface searchQuery_search {
   __typename: "Podcast";
   artist: string;
   episodesCount: number;
@@ -17,11 +47,11 @@ export interface SearchQuery_search {
   podcastName: string;
 }
 
-export interface SearchQuery {
-  search: SearchQuery_search[];
+export interface searchQuery {
+  search: searchQuery_search[];
 }
 
-export interface SearchQueryVariables {
+export interface searchQueryVariables {
   term: string;
 }
 
