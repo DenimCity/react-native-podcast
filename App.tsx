1c7732f7bc5326e32ed0fcf61145a311f3cb78d5
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-15 13:25:59 -0400

    added player to app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {UtilityThemeProvider} from 'react-native-design-utility';
+import React, {useState} from 'react';
+import {UtilityThemeProvider, Box} from 'react-native-design-utility';
 import {NavigationContainer} from '@react-navigation/native';
 import {ApolloProvider} from '@apollo/client';
+import TrackPlayer from 'react-native-track-player';
 
 import {theme} from './src/constants/theme';
 import MainStackNavigator from './src/navigators/MainStackNavigator';
 import client from './src/graphql/client';
+import trackPlayerService from './src/services/trackPlayerServices';
+import {ActivityIndicator} from 'react-native';
+import {PlayerContextProvider} from './src/context/PlayerContext';
 
 const App = () => {
+  const [isReady, setIsReady] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    TrackPlayer.setupPlayer().then(() => {
+      console.log('player is setup');
+      TrackPlayer.registerPlaybackService(() => trackPlayerService);
+      setIsReady(true);
+    });
+  }, []);
   return (
     <UtilityThemeProvider theme={theme}>
       <ApolloProvider client={client}>
-        <NavigationContainer>
-          <MainStackNavigator />
-        </NavigationContainer>
+        {isReady ? (
+          <PlayerContextProvider>
+            <NavigationContainer>
+              <MainStackNavigator />
+            </NavigationContainer>
+          </PlayerContextProvider>
+        ) : (
+          <Box f={1} center>
+            <ActivityIndicator />
+          </Box>
+        )}
       </ApolloProvider>
     </UtilityThemeProvider>
   );
