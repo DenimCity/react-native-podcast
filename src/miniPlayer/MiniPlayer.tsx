83aae04e9a894ad0ff203f311c8c193397b0b9c0
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-15 13:27:17 -0400

    created mini player

---
+++ src/miniPlayer/MiniPlayer.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {Box, Text} from 'react-native-design-utility';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {usePlayerContext} from '../context/PlayerContext';
+import {theme} from '../constants/theme';
+import {Image} from 'react-native';
+
+const MiniPlayer = () => {
+  const playerContext = usePlayerContext();
+
+  if (playerContext.isEmpty || !playerContext.currentTrack) {
+    return null;
+  }
+  return (
+    <Box
+      h={75}
+      bg="white"
+      px="sm"
+      style={{borderTopWidth: 1, borderTopColor: theme.color.greyLightest}}>
+      <Box f={1} dir="row" align="center" justify="between">
+        <Box
+          h={50}
+          w={50}
+          bg="blueLight"
+          radius={10}
+          mr={10}
+          style={{overflow: 'hidden'}}>
+          <Image
+            source={{uri: playerContext.currentTrack.artwork}}
+            style={{flex: 1}}
+          />
+        </Box>
+        <Box f={1} mr={20}>
+          <Text numberOfLines={1}>{playerContext.currentTrack.title}</Text>
+        </Box>
+        <Box>
+          {playerContext.isPaused && (
+            <Box>
+              <TouchableOpacity onPress={() => playerContext.play()}>
+                <Icon name="play" size={30} />
+              </TouchableOpacity>
+            </Box>
+          )}
+          {playerContext.isPlaying && (
+            <Box>
+              <TouchableOpacity onPress={() => playerContext.pause()}>
+                <Icon name="pause" size={30} />
+              </TouchableOpacity>
+            </Box>
+          )}
+          {playerContext.isStopped && (
+            <Box>
+              <TouchableOpacity onPress={() => null}>
+                <Icon name="square" size={30} />
+              </TouchableOpacity>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default MiniPlayer;
