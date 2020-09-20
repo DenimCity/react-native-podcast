14dc958f2ea6969af0b6624fe118248c2e8d9f69
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-20 14:53:50 -0400

    updated player and mini player

--- src/miniPlayer/MiniPlayer.tsx
+++ src/miniPlayer/MiniPlayer.tsx
@@ -1,62 +1,62 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native-gesture-handler';
 import {Box, Text} from 'react-native-design-utility';
 import Icon from 'react-native-vector-icons/Feather';
+import {Image, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 import {usePlayerContext} from '../context/PlayerContext';
 import {theme} from '../constants/theme';
-import {Image} from 'react-native';
 
 const MiniPlayer = () => {
   const playerContext = usePlayerContext();
+  const navigation = useNavigation();
 
   if (playerContext.isEmpty || !playerContext.currentTrack) {
     return null;
   }
+
   return (
-    <Box
-      h={75}
-      bg="white"
-      px="sm"
-      style={{borderTopWidth: 1, borderTopColor: theme.color.greyLightest}}>
-      <Box f={1} dir="row" align="center" justify="between">
-        <Box
-          h={50}
-          w={50}
-          bg="blueLight"
-          radius={10}
-          mr={10}
-          style={{overflow: 'hidden'}}>
-          <Image
-            source={{uri: playerContext.currentTrack.artwork}}
-            style={{flex: 1}}
-          />
-        </Box>
-        <Box f={1} mr={20}>
-          <Text numberOfLines={1}>{playerContext.currentTrack.title}</Text>
-        </Box>
-        <Box mr={10}>
-          {playerContext.isPaused ? (
-            <TouchableOpacity onPress={() => playerContext.play()}>
-              <Icon name="play" size={30} />
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity onPress={() => playerContext.pause()}>
-              <Icon name="pause" size={30} />
-            </TouchableOpacity>
-          )}
-        </Box>
-        <Box>
-          <TouchableOpacity>
-            <Icon
-              size={30}
-              name="rotate-cw"
-              onPress={() => playerContext.seekTo()}
+    <TouchableOpacity onPress={() => navigation.navigate('Player')}>
+      <Box
+        h={75}
+        bg="white"
+        px="sm"
+        style={{borderTopWidth: 1, borderTopColor: theme.color.greyLightest}}>
+        <Box f={1} dir="row" align="center" justify="between">
+          <Box
+            h={50}
+            w={50}
+            bg="blueLight"
+            radius={10}
+            mr={10}
+            style={{overflow: 'hidden'}}>
+            <Image
+              source={{uri: playerContext.currentTrack.artwork}}
+              style={{flex: 1}}
             />
-          </TouchableOpacity>
+          </Box>
+          <Box f={1} mr={20}>
+            <Text numberOfLines={1}>{playerContext.currentTrack.title}</Text>
+          </Box>
+          <Box mr={10}>
+            {playerContext.isPaused ? (
+              <TouchableOpacity onPress={() => playerContext.play()}>
+                <Icon name="play" size={30} />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity onPress={playerContext.pause}>
+                <Icon name="pause" size={30} />
+              </TouchableOpacity>
+            )}
+          </Box>
+          <Box>
+            <TouchableOpacity onPress={() => playerContext.seekTo()}>
+              <Icon name="rotate-cw" size={30} />
+            </TouchableOpacity>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </TouchableOpacity>
   );
 };
 
