81e0b40bc927ddfd322043e1935dd97e3f4828fd
DenimCity <Jean.Altidor@coxautoinc.com>
2020-09-20 13:50:11 -0400

    rendering subscription in library

--- src/components/library/LibraryScreen.tsx
+++ src/components/library/LibraryScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {Box, Text} from 'react-native-design-utility';
+import {DBContext} from '../../context/DBContext';
 
 const LibraryScreen = () => {
+  const dbContext = React.useContext(DBContext);
   return (
-    <Box f={1} center>
-      <Text>Library Screen</Text>
+    <Box f={1}>
+      {dbContext.podcasts.map((podcast) => {
+        console.log('LibraryScreen -> podcast', podcast.name);
+        return (
+          <Box
+            key={`${podcast.artist}-${Math.random()}`}
+            bg="white"
+            mb="md"
+            p="sm">
+            <Text>{podcast.artist}</Text>
+          </Box>
+        );
+      })}
     </Box>
   );
 };
